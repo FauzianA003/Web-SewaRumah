244636407515b71201e228fabe9d73650a84bf4e
FauzianA003 <mohafshor@gmail.com>
2026-06-18 16:23:20 +0700

    Fix: Bind translator in AppServiceProvider for Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('translator', function ($app) {
+            return $app->make('translation.loader');
+        });
     }
 
     /**
